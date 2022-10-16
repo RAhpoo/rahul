8c378a177353005a6305906593da1b71b3727fbb
Rahul Jaiswal <106617099+RAhpoo@users.noreply.github.com>
2022-10-16 16:04:19 +0530

    14oct

---
+++ ScannerDemo.java
@@ -0,0 +1,17 @@
+class  ScannerDemo{
+public static void main(String args[]){
+	/*
+//declaring and initializing local variablle
+String name="Rahul jaiswal";
+char gender='M';
+float marks=68.19f;
+boolean pass=true;
+//printing the value of all variable
+System.out.println("Name: "+name);
+System.out.println("Gender: "+gender);
+System.out.println("Age: "+age);
+System.out.println("Marks: "+marks);
+System.out.println("Pass: "+pass);*/
+
+}
+}
