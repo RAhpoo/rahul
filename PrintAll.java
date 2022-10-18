aa89e8afbffec1162171d61c373048973001de87
Rahul Jaiswal <106617099+RAhpoo@users.noreply.github.com>
2022-10-18 21:37:35 +0530

    Delete PrintAll.java

--- PrintAll.java
+++
@@ -1,9 +0,0 @@
-class PrintAll{
-public static void main(String args[]){
-System.out.println("Welcome First "+args[0]);
-System.out.println("Welcome Last "+args[1]);
-System.out.println("Welcome Age "+args[2]);
-System.out.println("Welcome Course "+args[3]);
-System.out.println("Welcome score "+args[4]);
-}
-}
